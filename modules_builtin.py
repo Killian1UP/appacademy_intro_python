e1a725bbda67b859e4ce431e1d474cdd7266b8b9
Ikaelelo Motlhako <ikaelelo.motlhako@gmail.com>
2025-07-03 23:28:06 +0200

    Python file showcasing diffrent modules with different use.

---
+++ modules_builtin.py
@@ -0,0 +1,11 @@
+import math
+import random
+import datetime
+import os
+import sys
+
+print(math.pi)     # to receive the value of pi 
+print(random.randint(0, 1000))    # randomizes a number from 0 - 1000
+print(datetime.datetime.now())    # get the current date and time
+print(os.getcwd())    # get the current working directory
+print(sys.version)    # get the python version
